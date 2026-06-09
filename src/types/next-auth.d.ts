2844ed5621d16a90599dc970806d43b6cc4fca1c
John Tran <phuocthanh03062004@gmail.com>
2026-06-10 00:05:53 +0700

    #194.1 Fix bug dự án thêm refresh token

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -15,7 +15,9 @@ declare module "next-auth/jwt" {
     interface JWT {
         access_token: string,
         refresh_token: string,
-        user: IUser
+        user: IUser,
+        access_expire: number,
+        error: string
     }
 }
 
@@ -26,7 +28,9 @@ declare module "next-auth" {
     interface Session {
         access_token: string,
         refresh_token: string,
-        user: IUser
+        user: IUser,
+        access_expire: number,
+        error: string
     }
 
 
